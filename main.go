42256d9631709f037b464f5c3e00f8576b91002b
j4me5 <duongdt@sonek.vn>
2026-01-28 15:59:01 +0700

    add login api

--- main.go
+++ main.go
@@ -27,12 +27,12 @@ func main() {
 		c.Writer.Header().Set("Access-Control-Allow-Origin", "*")
 		c.Writer.Header().Set("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS")
 		c.Writer.Header().Set("Access-Control-Allow-Headers", "Content-Type, Authorization")
-		
+
 		if c.Request.Method == "OPTIONS" {
 			c.AbortWithStatus(204)
 			return
 		}
-		
+
 		c.Next()
 	})
 
